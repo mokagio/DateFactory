f56bc7358870124a26ef3b26d6b56772abc426b4
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-06-01 19:41:27 +1000

    🎵 Let's get this party started 🎵

---
+++ DateFactory/DateFactory.h
@@ -0,0 +1,19 @@
+//
+//  DateFactory.h
+//  DateFactory
+//
+//  Created by Giovanni on 1/06/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateFactory.
+FOUNDATION_EXPORT double DateFactoryVersionNumber;
+
+//! Project version string for DateFactory.
+FOUNDATION_EXPORT const unsigned char DateFactoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateFactory/PublicHeader.h>
+
+
---
+++ DateFactoryTests/DateFactoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  DateFactoryTests.swift
+//  DateFactoryTests
+//
+//  Created by Giovanni on 1/06/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+import XCTest
+@testable import DateFactory
+
+class DateFactoryTests: XCTestCase {
+
+  func testExample() {
+    XCTAssertTrue(true)
+  }
+}
